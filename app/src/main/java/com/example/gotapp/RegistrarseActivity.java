07b93d60a254ce19d72a73795009d6baef903241
SebaGodirio <sebastian.godirio@wearepsh.com>
2022-09-23 19:05:50 -0300

    Agregar iconos, rv y logica base de datos

--- app/src/main/java/com/example/gotapp/RegistrarseActivity.java
+++ app/src/main/java/com/example/gotapp/RegistrarseActivity.java
@@ -3,12 +3,17 @@ package com.example.gotapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.SQLException;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class RegistrarseActivity extends AppCompatActivity {
 
+    private EditText etUsuario;
+    private EditText etPassword;
+    private EditText etPasswordRepetida;
     private Button registrarseBtn;
 
     @Override
@@ -16,13 +21,34 @@ public class RegistrarseActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrarse);
 
+        etUsuario = findViewById(R.id.etr_usuario);
+        etPassword = findViewById(R.id.etr_password);
+        etPasswordRepetida = findViewById(R.id.etr_password_repetida);
+
+        String usuario = etUsuario.getText().toString();
+        String password = etPassword.getText().toString();
+
         registrarseBtn = findViewById(R.id.btn_registrarse_form);
         registrarseBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                try {
+                    registrar(usuario, password);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
                 Intent intent = new Intent(RegistrarseActivity.this, LoginActivity.class);
                 startActivity(intent);
             }
         });
+    }
+
+    private void registrar(String usuario, String password) throws Exception {
+        Login login = new Login( usuario, password);
+        try {
+            LoginManager.getInstancia(this).agregarLogin(login);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
